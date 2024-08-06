b9444df644c849ae707d8317b979a79d9ab97cc3
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-06 14:02:07 +0300

    IWriteRepositoryAdded

---
+++ CinemaTicketSystem.Application/IRepositories/IWriteRepository.cs
@@ -0,0 +1,35 @@
+﻿using CinemaTicketSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketSystem.Application.IRepositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        #region Insert
+        T Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        #endregion
+
+
+        #region Update
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        #endregion
+
+
+        #region Delete
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+        #endregion
+
+
+        #region Save
+        int Save();
+        #endregion
+    }
+}
