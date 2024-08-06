559e389f5293dfa860beda0cd10aa412b2e79bf6
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-06 14:02:55 +0300

    ICategoryReadRespositoryAdded

---
+++ CinemaTicketSystem.Application/IRepositories/CategoryRepositories/ICategoryReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using CinemaTicketSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketSystem.Application.IRepositories.CategoryRepositories
+{
+    public interface ICategoryReadRepository : IReadAsyncRepository<Category>, IReadRepository<Category>
+    {
+
+    }
+}
