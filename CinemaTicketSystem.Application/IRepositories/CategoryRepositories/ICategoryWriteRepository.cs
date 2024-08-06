ae1e96833f02e31b02bf618376b0c81ead788c76
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-06 14:03:14 +0300

    ICategoryWriteRepositoryAdded

---
+++ CinemaTicketSystem.Application/IRepositories/CategoryRepositories/ICategoryWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using CinemaTicketSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketSystem.Application.IRepositories.CategoryRepositories
+{
+    public interface ICategoryWriteRepository : IWriteAsyncRepository<Category>, IWriteRepository<Category>
+    {
+
+    }
+}
