0a25a4b0c661ce0ad1ef1da899835970c6605818
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-06 14:20:27 +0300

    CategoryRepositoriesAdded

---
+++ CinemaTicketSystem.DataAccess/Repositories/CategoryRepositories/CategoryReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using CinemaTicketSystem.Application.IRepositories;
+using CinemaTicketSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketSystem.DataAccess.Repositories.CategoryRepositories
+{
+    public class CategoryReadRepository : ReadAsyncRepository, IReadAsyncRepository<Category>, IReadRepository<Category>
+    {
+    }
+}
---
+++ CinemaTicketSystem.DataAccess/Repositories/CategoryRepositories/CategoryWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using CinemaTicketSystem.Application.IRepositories;
+using CinemaTicketSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketSystem.DataAccess.Repositories.CategoryRepositories
+{
+    public class CategoryWriteRepository : WriteAsyncRepository, IWriteAsyncRepository<Category>, IWriteRepository<Category>    
+    {
+    }
+}
