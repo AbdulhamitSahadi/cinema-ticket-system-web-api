695cea7bf04f743017b0c4b1b4603bcbc7e65f9f
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-06 09:37:39 +0300

    OrderDetailsDomainModelAdded

---
+++ CinemaTicketSystem.Domain/Entities/OrderDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketSystem.Domain.Entities
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+
+        public decimal Price { get; set; }
+
+
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        public int OrderId { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
