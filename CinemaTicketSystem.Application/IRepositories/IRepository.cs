b2ac17ec12e014f40111d14886544f295ece9bf0
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-06 14:02:23 +0300

    IRepositoryAdded

---
+++ CinemaTicketSystem.Application/IRepositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using CinemaTicketSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketSystem.Application.IRepositories
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> Table { get; }
+        IQueryable<T> TableNoTracking { get; }
+    }
+}
